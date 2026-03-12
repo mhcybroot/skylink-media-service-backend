cfab2fdd6dee93a81b34065ad739cb29a823bf3e
mhcybrooted <honey.app.developer@gmail.com>
2026-03-12 08:40:54 +0600

    feat: Initialize new Spring Boot project structure with Gradle.

---
+++ src/main/java/root/cyb/mh/skylink_media_service/SkylinkMediaServiceApplication.java
@@ -0,0 +1,13 @@
+package root.cyb.mh.skylink_media_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylinkMediaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkylinkMediaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/root/cyb/mh/skylink_media_service/SkylinkMediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package root.cyb.mh.skylink_media_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkylinkMediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
